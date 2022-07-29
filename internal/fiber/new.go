d12886509605db39a97c346eaaee85ce62302a17
WAAutoMaton <a547673135@gmail.com>
2022-07-29 15:51:04 +0800

    add fiber monitor

--- internal/fiber/new.go
+++ internal/fiber/new.go
@@ -2,9 +2,11 @@ package fiber
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
 func New() (app *fiber.App) {
 	app = fiber.New(getFiberConfig())
+	app.Get("/metrics_monitor", monitor.New())
 	return app
 }
