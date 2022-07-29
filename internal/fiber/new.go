eab148b9f153b62a513f2f331a7e3d5fdf2aa5d6
WAAutoMaton <a547673135@gmail.com>
2022-07-29 16:11:27 +0800

    增加fiber请求限流装置

--- internal/fiber/new.go
+++ internal/fiber/new.go
@@ -1,12 +1,26 @@
 package fiber
 
 import (
+	"coursebench-backend/pkg/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"time"
 )
 
 func New() (app *fiber.App) {
 	app = fiber.New(getFiberConfig())
 	app.Get("/metrics_monitor", monitor.New())
+	app.Use(limiter.New(limiter.Config{
+		Max: 60,
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).JSON(models.ErrorResponse{
+				Error:     true,
+				Errno:     "TooManyRequests",
+				Message:   "Too many requests, please try again a minute later",
+				Timestamp: time.Now(),
+			})
+		},
+	}))
 	return app
 }
