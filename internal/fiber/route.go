23672570825b0d889cec5676d1410de5a43db055
WAAutoMaton <a547673135@gmail.com>
2022-10-01 12:04:24 +0800

    disable test route in non-development mode

--- internal/fiber/route.go
+++ internal/fiber/route.go
@@ -1,6 +1,7 @@
 package fiber
 
 import (
+	"coursebench-backend/internal/config"
 	"coursebench-backend/internal/controllers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -10,7 +11,9 @@ func Routes(app *fiber.App) {
 	route := app.Group("/v1")
 	route.Use(cors.New())
 	controllers.UserRoutes(route)
-	controllers.TestRoutes(route)
+	if config.GlobalConf.InDevelopment {
+		controllers.TestRoutes(route)
+	}
 	controllers.CourseRoutes(route)
 	controllers.CommentRoutes(route)
 	controllers.TeacherRoute(route)
