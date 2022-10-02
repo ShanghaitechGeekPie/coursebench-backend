e08e3aa116d19eee8180b24042374a2bf4f24bbb
WAAutoMaton <a547673135@gmail.com>
2022-10-03 00:16:19 +0800

    fix: log format

--- internal/fiber/log_middleware.go
+++ internal/fiber/log_middleware.go
@@ -10,11 +10,11 @@ import (
 func LogMiddleware(c *fiber.Ctx) error {
 	s := "New Request: " + c.String()
 	if c.Path() == "/v1/user/login" || c.Path() == "/v1/user/register" || c.Path() == "/v1/user/update_password" || c.Path() == "/v1/user/reset_password_active" { // Don't log the password. parsing json is too much trouble here, so I just don't record the whole body.
-		s += " Body: Sensitive, skip..."
+		s += " ; Body: Sensitive, skip... "
 	} else if len(c.Body()) < 500 {
-		s += "; Body: " + string(c.Body())
+		s += " ; Body: " + string(c.Body())
 	} else {
-		s += fmt.Sprintf("; Body: Too long, %v bytes, skip...", len(c.Body()))
+		s += fmt.Sprintf(" ; Body: Too long, %v bytes, skip...", len(c.Body()))
 	}
 	log.Println(s)
 	return c.Next()
