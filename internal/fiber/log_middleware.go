fcb8c2991199b2f31c0c9193e3e00caa475fb901
WAAutoMaton <a547673135@gmail.com>
2022-10-03 00:15:27 +0800

    fix: not record password in log middleware

--- internal/fiber/log_middleware.go
+++ internal/fiber/log_middleware.go
@@ -9,7 +9,9 @@ import (
 // LogMiddleware record the details of each request.
 func LogMiddleware(c *fiber.Ctx) error {
 	s := "New Request: " + c.String()
-	if len(c.Body()) < 500 {
+	if c.Path() == "/v1/user/login" || c.Path() == "/v1/user/register" || c.Path() == "/v1/user/update_password" || c.Path() == "/v1/user/reset_password_active" { // Don't log the password. parsing json is too much trouble here, so I just don't record the whole body.
+		s += " Body: Sensitive, skip..."
+	} else if len(c.Body()) < 500 {
 		s += "; Body: " + string(c.Body())
 	} else {
 		s += fmt.Sprintf("; Body: Too long, %v bytes, skip...", len(c.Body()))
