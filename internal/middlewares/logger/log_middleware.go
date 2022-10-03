68fff3063c9825cefbd188144aa4ba4e6a4a1b7d
WAAutoMaton <a547673135@gmail.com>
2022-10-03 13:51:22 +0800

    add log

--- internal/middlewares/logger/log_middleware.go
+++ internal/middlewares/logger/log_middleware.go
@@ -4,11 +4,13 @@ import (
 	"coursebench-backend/pkg/log"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"strings"
 )
 
 // LogMiddleware record the details of each request.
 func LogMiddleware(c *fiber.Ctx) error {
 	s := "New Request: " + c.String()
+	s += " ; X-Forwarded-For: " + strings.Join(c.IPs(), ",")
 	if c.Path() == "/v1/user/login" || c.Path() == "/v1/user/register" || c.Path() == "/v1/user/update_password" || c.Path() == "/v1/user/reset_password_active" { // Don't log the password. parsing json is too much trouble here, so I just don't record the whole body.
 		s += " ; Body: Sensitive, skip... "
 	} else if len(c.Body()) < 500 {
