62f95d92ed1a0e6b1d7ac95d91ce77a4e07a0173
WAAutoMaton <a547673135@gmail.com>
2022-10-05 18:01:29 +0800

    Revert "debugging"
    This reverts commit ad506bc8506e36f353d20d7045b5b2401e931103.

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,21 +3,16 @@ package utils
 import (
 	"coursebench-backend/internal/config"
 	"github.com/gofiber/fiber/v2"
-	"log"
 )
 
 func GetIP(ctx *fiber.Ctx) []string {
 	if config.FiberConfig.UseXForwardFor {
-		log.Println("UseXForwardFor")
 		if len(ctx.IPs()) > 0 {
-			log.Println("UseXForwardFor", len(ctx.IPs()))
 			return ctx.IPs()
 		} else {
-			log.Println("UseXForwardFor, but GG")
 			return []string{ctx.IP()}
 		}
 	} else {
-		log.Println("Not UseXForwardFor")
 		return []string{ctx.IP()}
 	}
 }
