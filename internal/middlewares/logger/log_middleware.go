21d095c29da24fd8ec3c9be1d08994baf1ce3b6e
WAAutoMaton <a547673135@gmail.com>
2022-12-08 17:39:23 +0800

    放宽写入日志的body长度限制

--- internal/middlewares/logger/log_middleware.go
+++ internal/middlewares/logger/log_middleware.go
@@ -3,7 +3,6 @@ package logger
 import (
 	"coursebench-backend/pkg/log"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
 	"strings"
 )
 
@@ -13,7 +12,7 @@ func LogMiddleware(c *fiber.Ctx) error {
 	s += " ; X-Forwarded-For: " + strings.Join(c.IPs(), ",")
 	if c.Path() == "/v1/user/login" || c.Path() == "/v1/user/register" || c.Path() == "/v1/user/update_password" || c.Path() == "/v1/user/reset_password_active" { // Don't log the password. parsing json is too much trouble here, so I just don't record the whole body.
 		s += " ; Body: Sensitive, skip... "
-	} else if len(c.Body()) < 500 {
+	} else if len(c.Body()) < 2000 {
 		s += " ; Body: " + string(c.Body())
 	} else {
 		s += fmt.Sprintf(" ; Body: Too long, %v bytes, skip...", len(c.Body()))
